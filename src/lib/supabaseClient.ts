8159b776e503e3aa940e7de854b1594469f64097
divine-itura <divine@myitura.com>
2026-08-22 16:01:01 +0100

    updated experience4

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseAnonKey = process.env.SUPABASE_PUBLISHABLE_KEY || "";
+const supabaseUrl = process.env.SUPABASE_URL || process.env.SUPABASE_URL || "";
+const supabaseAnonKey = process.env.SUPABASE_PUBLISHABLE_KEY || process.env.SUPABASE_PUBLISHABLE_KEY || "";
 
 export const supabase = (supabaseUrl && supabaseAnonKey)
   ? createClient(supabaseUrl, supabaseAnonKey, {
