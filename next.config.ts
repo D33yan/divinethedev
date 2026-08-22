22b372d79f35308d17a98154eed4a22e2434a8ff
divine-itura <divine@myitura.com>
2026-08-22 16:08:37 +0100

    updated experience5

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL || "",
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY || "",
+    SUPABASE_URL: process.env.SUPABASE_URL || "",
+    SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY || "",
+  },
 };
 
 export default nextConfig;
